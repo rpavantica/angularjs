6d5dbb979ec292735985f243a12cc5688fa9c2cf
Raphael Polniak <rpolniak.ctr@thehonestcompany.com>
2019-02-18 18:40:43 -0600

    figured out routes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,31 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
+// 'ngRoute', 'ngResource'
+
+//routes
+
+myApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+
+    // route for the home page
+    .when('/page1', {
+        templateUrl : 'pages/page-one.html',
+        controller  : 'pageOneController'
+    })
+
+    // route for the about page
+    .when('/page2', {
+        templateUrl : 'pages/page-two.html',
+        controller  : 'pageTwoController'
+    })
+}]);
+
+
+myApp.controller('pageOneController',["$scope",function($scope){
+}]);
+
+
+myApp.controller('pageTwoController',["$scope",function($scope){
+}]);
 
 myApp.controller('mainController',["$scope",function($scope){
   $scope.cart = 'empty';
@@ -26,14 +53,15 @@ myApp.directive("searchResult", function() {
      personObject: "=",
      parsePersonFunction: "&"
     },
-    link: (scope, elements, attrs) => {
-      console.log('Linkinging');
-      console.log(elements); 
-      console.log(scope);
-      if (scope.personObject.firstName == 'gitza') {
-        elements.attr('class', 'blue');
-      }
-    },
+    transclude: true,
+    // link: (scope, elements, attrs) => {
+    //   console.log('Linkinging');
+    //   console.log(elements); 
+    //   console.log(scope);
+    //   if (scope.personObject.firstName == 'gitza') {
+    //     elements.attr('class', 'blue');
+    //   }
+    // }
   }        
 });
 
